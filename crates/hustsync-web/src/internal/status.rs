1d7e6863cfdfefcb6f38c6bc1e8e8568f3998795
Polardist <polardist@openatom.club>
2025-11-08 14:31:42 +0800

    feat: add tests for SyncStatus serialization and deserialization

--- crates/hustsync-web/src/internal/status.rs
+++ crates/hustsync-web/src/internal/status.rs
@@ -14,3 +14,17 @@ pub enum SyncStatus {
     #[serde(other)]
     Unknown,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::SyncStatus;
+    use serde_json;
+    #[test]
+    fn test_sync_status_json_ser_de() {
+        let b = serde_json::to_vec(&SyncStatus::PreSyncing).unwrap();
+        assert_eq!(b, b"\"pre-syncing\"");
+
+        let s: SyncStatus = serde_json::from_slice(b"\"failed\"").unwrap();
+        assert_eq!(s, SyncStatus::Failed);
+    }
+}
