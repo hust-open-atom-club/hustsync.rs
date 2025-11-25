d1a0e612f2cb1c1d0a8c371ef1ec718b36018c58
Polardist <polardist@openatom.club>
2025-11-25 18:50:48 +0800

    feat: add tempfile dependency and update tests for configuration loading

--- crates/hustsync-manager/tests/test_config.rs
+++ crates/hustsync-manager/tests/test_config.rs
@@ -1,10 +1,14 @@
 #![cfg(test)]
 
 use hustsync_config_parser::ManagerConfig;
+use std::fs;
+use std::io::Write;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+use tempfile::NamedTempFile;
 
-#[test]
-fn toml_decoding_should_work() {
-    let cfg_blob = r#"
+fn make_cfg_blob() -> &'static str {
+    r#"
 debug = true
 [server]
 addr = "0.0.0.0"
@@ -13,8 +17,12 @@ port = 5000
 [files]
 status_file = "/tmp/tunasync.json"
 db_file = "/var/lib/tunasync/tunasync.db"
-"#;
+"#
+}
 
+#[test]
+fn toml_decoding_should_work() {
+    let cfg_blob = make_cfg_blob();
     let cfg: ManagerConfig = toml::from_str(cfg_blob).expect("decode toml");
     assert_eq!(
         cfg.server.as_ref().unwrap().addr.as_ref().unwrap(),
@@ -25,4 +33,19 @@ db_file = "/var/lib/tunasync/tunasync.db"
         cfg.files.as_ref().unwrap().db_file.as_ref().unwrap(),
         "/var/lib/tunasync/tunasync.db"
     );
+}
+
+// TODO 创建 clap 应用以测试更完整的加载流程
+// 需要对应 tunasync config_test 部分
+#[test]
+fn toml_loading_should_work() {
+    let cfg_blob = make_cfg_blob();
+    let mut tmp = NamedTempFile::new().expect("create tempfile");
+    tmp.write_all(cfg_blob.as_bytes()).expect("write tempfile");
+    tmp.flush().expect("flush tempfile");
+
+    #[cfg(unix)]
+    fs::set_permissions(tmp.path(), fs::Permissions::from_mode(0o644)).expect("set perms");
+
+    // let tempfile_path = tmp.path().to_path_buf();
 }
